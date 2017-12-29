e4043591b389848d01e36481492e7ff288abee43
Sagar <sagar.writeme@gmail.com>
2017-12-29 17:37:09 +0530

    get canvas size from emscripten

--- html5/build.sh
+++ html5/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 emcc -Wall -O2 -s USE_GLFW=3 -s TOTAL_MEMORY=67108864 -Iinc \
-    src/platform.c \
+    src/platform.c src/game.c \
     -o bin/kage.html
--- html5/src/platform.c
+++ html5/src/platform.c
@@ -9,13 +9,50 @@
 #include <GLFW/glfw3.h>
 
 #include "platform.h"
+#include "game.h"
 
+int width, height;
 GLFWwindow* window;
+
+GameState *game_state;
 
 void loop()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    GameInputEventType event_type = GAME_INPUT_EVENT_NULL;
+    float x = 0, y = 0;
+
+    glfwPollEvents();
+    if((glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) ||
+        (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)) {
+        event_type = GAME_INPUT_EVENT_SWIPE_UP;
+    }
+    if((glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) ||
+         (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)) {
+        event_type = GAME_INPUT_EVENT_SWIPE_DOWN;
+    }
+    if((glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) ||
+         (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)) {
+        event_type = GAME_INPUT_EVENT_SWIPE_LEFT;
+    }
+    if((glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) ||
+         (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)) {
+        event_type = GAME_INPUT_EVENT_SWIPE_RIGHT;
+    }
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        event_type = GAME_INPUT_EVENT_PAUSE;
+    }
+    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+        event_type = GAME_INPUT_EVENT_PRESS;
+        double xd, yd;
+        glfwGetCursorPos(window, &xd, &yd);
+        x = (float) (xd/width);
+        y = (float) (yd/height);
+    }
+    GameInput game_input = {event_type, x, y};
+
+    GameStateUpdate(game_state, game_input);
+    GameRender(game_state);
+
     glfwSwapBuffers(window);
 }
 
@@ -26,7 +63,9 @@ int main()
         return 1;
     }
 
-    int width = 960, height = 540;
+    emscripten_get_canvas_size(&width, &height, NULL);
+    LOGI("Window size -- w: %d, h: %d\n", width, height);
+
     glfwWindowHint(GLFW_SAMPLES, 4);
     window = glfwCreateWindow(width, height, "Kage", NULL, NULL);
     if (!window)
@@ -37,6 +76,9 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+
+    game_state = (GameState*) calloc(1, sizeof(GameState));
+    GameInit(game_state);
 
     LOGI("Starting main loop\n");
     emscripten_set_main_loop(loop, 0, 1);
