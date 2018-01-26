f0cb18e9557f5f1a37aca89d4240d04182a11bb6
Sagar Gubbi <sagar.writeme@gmail.com>
2018-01-26 17:55:38 +0530

    JNI wrapper

--- android/build.sh
+++ android/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+mkdir -p build
+javac -source 1.6 -target 1.6 -bootclasspath /usr/lib/jvm/java-6-oracle/jre/lib/rt.jar -d ./build java/com/sagargv/kage/GameWrapper.java
+
 NDK_PROJECT_PATH=. ndk-build NDK_APPLICATION_MK=./Application.mk
+
+mv libs lib
+jar cf kage.jar lib -C build .
+mv lib libs
+rm -rf build
---
+++ android/java/com/sagargv/kage/GameWrapper.java
@@ -0,0 +1,23 @@
+// Copyright (c) 2017 Kage authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package com.sagargv.kage;
+
+public class GameWrapper {
+    static {
+        System.loadLibrary("kage");
+    }
+
+    public static native void setDataDir(String dataDir);
+
+    public static native int getGameStateSize();
+    public static native byte[] getGameState();
+    public static native void setGameState(byte[] gameState);
+
+    public static native int audioInit();
+    public static native int graphicsInit();
+
+    public static native void gameReset();
+    public static native void gameLoop(int width, int height, int eventType, float x, float y);
+}
